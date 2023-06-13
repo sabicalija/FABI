79902df3ca9b9e39b61735872bbb3042f6a71373
Anton Altmeyer <anton.altmeyer002@stud.fh-dortmund.de>
2023-06-13 16:28:40 +0200

    Adding functions for Arduino RP2040

--- FabiWare/display.cpp
+++ FabiWare/display.cpp
@@ -23,19 +23,22 @@
 */
 void initDisplay(){
 
-      // Select the font to use with menu and all font functions
-      // Note the the font selection heavily influences program memory utilisation!
+  // Select the font to use with menu and all font functions
+  // Note the the font selection heavily influences program memory utilisation!
       
-#if defined(ARDUINO_AVR_MICRO)
-      //ssd1306_setFixedFont(ssd1306xled_font6x8);
-      ssd1306_setFixedFont(ssd1306xled_font8x16);
-
-      ssd1306_128x32_i2c_init();
-      ssd1306_clearScreen();
-
-      //ssd1306_print("Normal text");
-      ssd1306_drawXBitmap(36, 0, 55, 32, FABIlogo);
-#endif
+  #if defined(ARDUINO_AVR_MICRO)
+    //ssd1306_setFixedFont(ssd1306xled_font6x8);
+    ssd1306_setFixedFont(ssd1306xled_font8x16);
+    ssd1306_128x32_i2c_init();
+    ssd1306_clearScreen();
+    //ssd1306_print("Normal text");
+    ssd1306_drawXBitmap(36, 0, 55, 32, FABIlogo);
+  #elif defined(ARDUINO_NANO_RP2040_CONNECT)
+    Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+    display.clearDisplay();
+    display.drawXBitmap((display.width()  - LOGO_WIDTH ) / 2, (display.height() - LOGO_HEIGHT) / 2, FABIlogo, LOGO_WIDTH, LOGO_HEIGHT, 1);
+    display.display();
+  #endif
 }
 
 /**
@@ -46,17 +49,30 @@ void initDisplay(){
    writes the slot number and BT/USB mode indicator to the LCD
 */
 void writeSlot2Display(){ 
-#if defined(ARDUINO_AVR_MICRO)
-  ssd1306_clearScreen();
-
-  for(uint8_t i=0; i < actSlot; i++)
-    ssd1306_printFixed(i*10, 0, "*", STYLE_NORMAL);
-
-  switch(settings.bt){
-    case 3:  ssd1306_printFixed(60, 0, "(USB+BT)", STYLE_NORMAL);  break;
-    case 1:  ssd1306_printFixed(85, 0, "(USB)", STYLE_NORMAL);  break;
-    case 2:  ssd1306_printFixed(90, 0, "(BT)", STYLE_NORMAL);  break;
-  }
-  ssd1306_printFixed(0, 20, settings.slotname, STYLE_NORMAL);
-#endif
+  #if defined(ARDUINO_AVR_MICRO)
+    ssd1306_clearScreen();
+    for(uint8_t i=0; i < actSlot; i++)
+      ssd1306_printFixed(i*10, 0, "*", STYLE_NORMAL);
+    switch(settings.bt){
+      case 3:  ssd1306_printFixed(60, 0, "(USB+BT)", STYLE_NORMAL);  break;
+      case 1:  ssd1306_printFixed(85, 0, "(USB)", STYLE_NORMAL);  break;
+      case 2:  ssd1306_printFixed(90, 0, "(BT)", STYLE_NORMAL);  break;
+    }
+    ssd1306_printFixed(0, 20, settings.slotname, STYLE_NORMAL);
+    
+  #elif defined(ARDUINO_NANO_RP2040_CONNECT)
+    Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+    display.clearDisplay();  
+    for(uint8_t i=0; i < actSlot; i++)
+      display.setCursor(i*10, 0);
+      display.print("*");
+    switch(settings.bt){
+      case 3:  display.setCursor(60, 0); display.print("(USB+BT)");  break;
+      case 1:  display.setCursor(85, 0); display.print("(USB)");  break;
+      case 2:  display.setCursor(90, 0); display.print("(BT)");  break;
+    }  
+    display.setCursor(0, 20);
+    display.print(settings.slotname);
+    display.display();
+  #endif
 }
