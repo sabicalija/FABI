1bf7fce3025d0130ea1d3ba91611b812ee7d438b
Anton Altmeyer <anton.altmeyer002@stud.fh-dortmund.de>
2023-06-13 16:28:40 +0200

    Adding functions for Arduino RP2040

--- FabiWare/display.h
+++ FabiWare/display.h
@@ -18,6 +18,19 @@
 
 #if defined(ARDUINO_AVR_MICRO)
 #include "ssd1306.h"    //  display Lib
+
+#elif defined(ARDUINO_NANO_RP2040_CONNECT)
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define LOGO_HEIGHT   32
+#define LOGO_WIDTH    55
+
 #endif
 
 void initDisplay();
